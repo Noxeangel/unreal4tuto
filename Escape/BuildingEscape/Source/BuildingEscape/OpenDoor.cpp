3bde530051b7ee51fad06fac825c91fbe104dd9a
Noxeangel <olivier.allouard@gmail.com>
2017-06-20 00:32:54 -0400

    Adding two first components to read and access transforms

---
+++ Escape/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+	AActor* Owner = GetOwner();
+	FString doorRotation = Owner->GetTransform().GetRotation().ToString();
+
+	FRotator NewRotation = FRotator(0.0f,60.0f,0.0f);
+
+	Owner->SetActorRotation(NewRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("door rotation is %s"), *doorRotation);
+	// ...
+	
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
